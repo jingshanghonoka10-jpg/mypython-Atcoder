17be5aa1eae123768d77212e48a26215b12eb1fe
Niko <jingshanghonoka10@gmail.com>
2026-03-13 14:54:31 +0900

    不要なフォルダを削除して整理

--- 最初の練習/While01.py
+++
@@ -1,8 +0,0 @@
-while True:
-    ans = int(input())
-    if ans == 7:
-        print('Collect!')
-        break
-    else:
-        print('Try again!')
-        
--- 最初の練習/if.py
+++
@@ -1,9 +0,0 @@
-
-drinks = input()
-
-if drinks == 'water':
-    print('Hard to drink plain...')
-elif drinks in ['coffee','tea']:
-    print('Perfect with sweets!')
-else:
-    print('Okay')
