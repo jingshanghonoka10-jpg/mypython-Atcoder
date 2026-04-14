f2d1889b356757c0888921f4555704a300eacb76
Niko <jingshanghonoka10@gmail.com>
2026-04-14 12:33:23 +0900

    センサーデータの更新問題

---
+++ ABC-453-B.py
@@ -0,0 +1,10 @@
+X, T = map(int,input().split())
+A = list(map(int,input().split()))
+
+value = A[0]
+print(0, A[0])
+
+for i in range(1, X+1):
+    if T <= abs(value - A[i]):
+        print(i, A[i])
+        value = A[i]
